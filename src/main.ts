79c2b76c4bb1228f440c74bd418e0455ac82fbc7
Vincent Huang <vincent.yh.huang@gmail.com>
2022-02-01 22:58:34 -0600

    Set up pegjs parser & error messages

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,45 @@
 import './style.css';
 import parse from './parser/parser';
 
-const defaultTestValue = 
-`
-LONDON, 1872
-Residence of Monsieur Phileas Fogg.
--> london
-
-=== london ===
-Monsieur Phileas Fogg returned home early from the Reform Club, and in a new-fangled steam-carriage, besides!  
-"Passepartout," said he. "We are going around the world!"
-
-+ "Around the world, Monsieur?"
-    I was utterly astonished. 
-    -> astonished
-+ [Nod curtly.] -> nod
-
-
-=== astonished ===
-"You are in jest!" I told him in dignified affront. "You make mock of me, Monsieur."
-"I am quite serious."
-
-+ "But of course"
-    -> ending
-
-
-=== nod ===
-I nodded curtly, not believing a word of it.
--> ending
-
-
-=== ending
-"We shall circumnavigate the globe within eighty days." He was quite calm as he proposed this wild scheme. "We leave for Paris on the 8:25. In an hour."
--> END`;
-
 const parsedTextField = document.getElementById("parsed-text");
+const errorField = document.getElementById("error-message");
+
+fetch('test-sentence.txt').then(response => {
+	return response.text();
+}).then(data => {
+	const textToParse = document.getElementById("text-to-parse");
+	if (textToParse !== null)
+		textToParse.textContent = data;
+	textToParse?.addEventListener('input', updateText);
+
+	parse(data).then(parseHandler, parseErrorHandler);
+});
+
 function updateText (this: HTMLInputElement, e : Event) {
-	const finalString : string = parse(this.value);
-	if (parsedTextField !== null)
-		parsedTextField.textContent = finalString;
+	parse(this.value).then(parseHandler, parseErrorHandler);
 }
 
-const textToParse = document.getElementById("text-to-parse");
-if (textToParse !== null)
-	textToParse.textContent = defaultTestValue;
-textToParse?.addEventListener('input', updateText);
+function parseHandler(textContent: string) {
+	if (parsedTextField !== null)
+	{
+		parsedTextField.textContent = textContent; 
+	}
+	if (errorField !== null)
+	{
+		errorField.textContent = '';
+	}
+}
 
-if (parsedTextField !== null)
-	parsedTextField.textContent = parse(defaultTestValue);
+function parseErrorHandler(error: any) {
+	if (errorField !== null)
+	{
+		errorField.innerHTML = 
+			`<div>Error found at ${error.location}</div>
+			<div>${error.message}</div>`;
+	}
+	if (parsedTextField !== null)
+	{
+		parsedTextField.textContent = ''; 
+	}
+}
+
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,10 +1,19 @@
-import pegjs from "pegjs";
+import pegjs, { Parser } from "pegjs";
 
-function parse(str : string | null) {
+let parser: Parser | undefined;
+
+async function parse(str : string | null) {
+	if (parser === undefined)
+	{
+		const grammar = 
+			await fetch('grammar.pegjs').then((response) => response.text());
+		parser = pegjs.generate(grammar);
+	}
+
 	if (str === null)
 	{
 		return '';
 	}
-	return str.toLocaleUpperCase();
+	return parser.parse(str);
 }
 export default parse;
